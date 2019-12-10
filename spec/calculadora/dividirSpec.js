3d18059792ad86914da609a7cc9798f253bfc8da
dgmodesto <douglasgomesmodesto@gmail.com>
2019-12-09 22:41:56 -0300

    feat(subtrair): implementar funcionalidade de dividir

---
+++ spec/calculadora/dividirSpec.js
@@ -0,0 +1,27 @@
+describe('Suíte de testes de divisão', () => {
+  const Calculadora = require('../../src/js/calculadora.js');
+
+  it('deve retornar 6 para 12 e 2', () => {
+    expect(Calculadora.dividir(12, 2)).toEqual(6);
+  });
+
+  it('deve retornar 6 para 12 e 2 em formato texto', () => {
+    expect(Calculadora.dividir('12', '2')).toEqual(6);
+  });
+
+  it('deve retornar 1.5 para 4.5 e 3', () => {
+    expect(Calculadora.dividir(4.5, 3)).toEqual(1.5);
+  });
+
+  it('deve retornar 1.5 para 4.5 e 3 em formato texto', () => {
+    expect(Calculadora.dividir('4.5', '3')).toEqual(1.5);
+  });
+
+  it('deve retornar 0 para valor 1 inválido', () => {
+    expect(Calculadora.dividir(undefined, 10)).toEqual(0);
+  });
+
+  it('deve retornar 0 para valor 2 inválido', () => {
+    expect(Calculadora.dividir(10, undefined)).toEqual(0);
+  });
+});
--- src/js/calculadora.js
+++ src/js/calculadora.js
@@ -24,10 +24,23 @@ var Calculadora = {
   },
 
   dividir: function(num1, num2) {
+
+    if (isNaN(num1) || isNaN(num2)) {
+      return 0;
+    }
+
+    num1 = parseFloat(num1);
+    num2 = parseFloat(num2);
     return num1 / num2;
   },
 
   multiplicar: function(num1, num2) {
+    if (isNaN(num1) || isNaN(num2)) {
+      return 0;
+    }
+
+    num1 = parseFloat(num1);
+    num2 = parseFloat(num2);    
     return num1 * num2;
   }
 };
