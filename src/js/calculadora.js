e26306741dee372254ff17a63c62e4f7e7324d71
dgmodesto <douglasgomesmodesto@gmail.com>
2019-12-09 23:14:40 -0300

    refactor(calculadora): refatorar lógica para realizar calculo conforme operação

--- src/js/calculadora.js
+++ src/js/calculadora.js
@@ -1,53 +1,56 @@
 var Calculadora = {
+  ADICAO: '+',
+  SUBTRACAO: '-',
+  DIVISAO: '/',
+  MULTIPLICACAO: '*',
+
+
   adicionar: function(num1, num2) {
-
-    if (isNaN(num1) || isNaN(num2)) {
-      return 0;
-    }
-
-    num1 = parseFloat(num1);
-    num2 = parseFloat(num2);
-
-
-    return num1 + num2;
+    return Calculadora.calcular(num1, num2, Calculadora.ADICAO);
   },
 
   subtrair: function(num1, num2) {
-    if (isNaN(num1) || isNaN(num2)) {
-      return 0;
-    }
-
-    num1 = parseFloat(num1);
-    num2 = parseFloat(num2);
-
-    return num1 - num2;
+    return Calculadora.calcular(num1,num2, Calculadora.SUBTRACAO);
   },
 
-  dividir: function(num1, num2) {
-
-    if (isNaN(num1) || isNaN(num2)) {
-      return 0;
-    }
-
-    if (!num2) {
-      return 'Erro';
-    }
-
-    num1 = parseFloat(num1);
-    num2 = parseFloat(num2);
-    return num1 / num2;
+  dividir: function(num1, num2) { 
+    return Calculadora.calcular(num1,num2, Calculadora.DIVISAO);
   },
 
   multiplicar: function(num1, num2) {
-    if (isNaN(num1) || isNaN(num2) || !num2) {
+    return Calculadora.calcular(num1,num2, Calculadora.MULTIPLICACAO);
+  },
+
+  calcular: function (num1, num2, operacao) {
+    let resultado = 0;
+    num1 = parseFloat(num1);
+    num2 = parseFloat(num2);
+
+    if (isNaN(num1) || isNaN(num2)) {
       return 0;
     }
-    
-    num1 = parseFloat(num1);
-    num2 = parseFloat(num2);   
 
-    return num1 * num2;
+    switch (operacao) {
+      case Calculadora.ADICAO:
+        resultado = num1 + num2;
+        break;
+      case Calculadora.SUBTRACAO:
+        resultado = num1 - num2;
+        break;
+      case Calculadora.DIVISAO:
+        if (!num2)
+          return 'Erro';
+        resultado = num1 / num2;
+        break;
+      case Calculadora.MULTIPLICACAO:
+        resultado = num1 * num2;
+        break;
+      default:
+        resultado = 0;
+    }
+    return resultado;
   }
+
 };
 
 
